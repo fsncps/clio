9c978a6b073e38f5054970fe3ae62444fca1d498
fsncps <fsncps@eml.cc>
2025-06-21 10:45:19 +0200

    console widget

--- clio/ui/widgets/log_widget.py
+++ clio/ui/widgets/log_widget.py
@@ -1,4 +1,6 @@
 from textual.widgets import RichLog
+from textual.widgets import Input, Static
+from textual.containers import Horizontal, Vertical
 
 class LoggerWidget(RichLog):
     """A widget for displaying application logs in real-time."""
@@ -13,3 +15,29 @@ class LoggerWidget(RichLog):
         """Append log content to the logger."""
         self.write(content)
 
+
+class ClioConsole(Vertical):
+    """A console widget with a logger display and command input."""
+    # CSS_PATH = ["console.css"]
+
+    def compose(self):
+        yield Vertical(
+                LoggerWidget(id=f"{self.id}-log", classes="logger"),
+                Horizontal(
+                    Static("",classes="PS1"),
+                    Input(placeholder="Enter command...", id=f"{self.id}-input", classes="prompt"),      classes="prompt-line"),
+                classes="console")
+
+    def on_mount(self):
+        self.logger = self.query_one(f"#{self.id}-log", expect_type=LoggerWidget)
+        self.input = self.query_one(f"#{self.id}-input", expect_type=Input)
+        self.input.focus()
+
+    async def on_input_submitted(self, event: Input.Submitted) -> None:
+        from ...utils.log_util import log_message
+        command = event.value.strip()
+        if command:
+            message = f"command `{command}` not found"
+            log_message(message)
+            self.logger.write_log(f"[b red]{message}[/]")
+        self.input.value = ""
